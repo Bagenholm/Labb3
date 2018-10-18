bcea62139a3243921145529f18174f4181307cfc
Patrik <sputtrosa@gmail.com>
2018-10-18 13:07:32 +0200

    Adding save as png functionality through strategy pattern

--- src/sample/pngSaveStrategy.java
+++ src/sample/pngSaveStrategy.java
@@ -1,9 +1,26 @@
 package sample;
 
-public class pngSaveStrategy implements SaveStrategy {
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
+public class PNGSaveStrategy implements SaveStrategy {
 
     @Override
-    public void save() {
-
+    public void save(File file, Canvas canvas) {
+        if(file != null){
+            WritableImage wi = new WritableImage((int)canvas.getWidth(), (int)canvas.getHeight() /*(int)WIDTH,(int)HEIGHT */);
+            try {
+                ImageIO.write(SwingFXUtils.fromFXImage(canvas.snapshot(null,wi),null),"png",file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
 }
